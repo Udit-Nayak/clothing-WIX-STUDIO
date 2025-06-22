12696b7b1701e6121ad3f35adce532ed167b6347
Udit <uditnayak1611@gmail.com>
2025-06-22 22:25:26 +0530

    login page completed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -29,7 +29,7 @@ const LoginPage = () => {
       ? "Register"
       : mode === MODE.RESET_PASSSWORD
       ? "Reset your password"
-      : "Verify your Email and";
+      : "Verify your Email";
 
   const buttonTitle =
     mode === MODE.LOGIN
